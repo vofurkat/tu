c794fe059747f76264c85ffd7c47c6fe4b301555
vofurkat <vfurkat@gmail.com>
2016-10-19 21:25:21 +0400

    ready index page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,44 @@
 $(function() {
 
-	// Custom JS
+	// $.pixlayout({clip: true, src: "img/layout.jpg", show:true, center:true, top:0, left:0, pervious: false});
+
+	// product img resize
+	$('.products-cats .product').imgLiquid();
+	// $('.special-offer .offers-box .offer-img').imgLiquid();
+
+	// open mobil menu
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		// $(".header-main .header-mnu ul").toggleClass("show-mnu");
+		$(".header-main .header-mnu ul").slideToggle('slow');
+	});
+
+
+	// animate far label + input
+	$('input').each(function() {
+		$(this).on('focus', function() {
+			$(this).parent('.css').addClass('active');
+		});
+
+		$(this).on('blur', function() {
+			if ($(this).val().length == 0) {
+				$(this).parent('.css').removeClass('active');
+			}
+		});
+
+		if ($(this).val() != '') $(this).parent('.css').addClass('active');
+
+	});
+
+	//owlCarousel slider header
+	$(".comment-slider").owlCarousel({
+		items: 1,
+		dots: true,
+		autoplay: true,
+		loop: true
+	});
+
+	// dotdotdot
+  	$(".special-offer .offers-box .offer .offer-desc").dotdotdot();
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,13 @@ gulp.task('headersass', function() {
 gulp.task('libs', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		// 'app/libs/magnific-popup/magnific-popup.min.js'
+		'app/libs/bootsrap/bootstrap.js',
+		'app/libs/waypoints/waypoints.js',
+		'app/libs/wow/wow.js',
+		'app/libs/pixLayout/jquery.pixlayout.0.9.7.js',
+		'app/libs/image-resizing/imgLiquid-min.js',
+		'app/libs/dotdotdot-master/jquery.dotdotdot.js',
+		'app/libs/owlcarousel/owl.carousel.min.js'
 		])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
